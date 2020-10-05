d46f1098875ff61f492a533c74bdc562251394a7
Munteanu Ion <king6565@hp-pavilion-laptop-15.zte.com.cn>
2020-10-05 10:18:49 +0300

    Adaugarea noilor date la obiectul de tip class Laptop cu ajutorul setter si java.util.Scanner

--- src/md/utm/Main.java
+++ src/md/utm/Main.java
@@ -1,5 +1,7 @@
 package md.utm;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,9 +9,23 @@ public class Main {
             "GHz", 8, "DDR4", 256, "GB", "Ubuntu 20.04");
         System.out.println(pc1.printall());
         System.out.println("\n***************************************");
-        pc1.setPretul(9500);
-        pc1.setAnul(2020);
-        pc1.setRam(16);
+
+        Scanner var = new Scanner(System.in);
+        int var1;
+
+        System.out.println("Introduceti Pretul nou al produsului: \n");
+        var1 = var.nextInt();
+        pc1.setPretul(var1);
+
+        System.out.println("Introduceti Anul nou al produsului \n");
+        var1 = var.nextInt();
+        pc1.setAnul(var1);
+
+        System.out.println("Introduceti capacitatea Ram noua al produsului: \n");
+        var1 = var.nextInt();
+        pc1.setRam(var1);
+
+        System.out.println("\n***************************************");
         System.out.println(pc1.printall());
         System.out.println("\n***************************************");
         System.out.println(pc1.getSistemOp() + "\n" + pc1.getProcesor() + "\n" + pc1.getMarca());
